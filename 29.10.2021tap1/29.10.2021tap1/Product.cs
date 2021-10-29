5eaf19d494940294da1b4aacc008c6f6ac14c3c7
10hesen044 <tu201906250@code.edu.az>
2021-10-29 11:34:52 +0400

    project added

---
+++ 29.10.2021tap1/29.10.2021tap1/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _29._10._2021tap1
+{
+    class Product
+    {
+        public string Name;
+        public string BrandName;
+        public double Price;
+    }
+}
---
+++ 29.10.2021tap1/29.10.2021tap1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _29._10._2021tap1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product();
+
+            product.Name = "Sweatshirt";
+            product.BrandName = "Tommy Hilfiger";
+            product.Price = 150;
+
+            Console.WriteLine($"{product.Name} {product.BrandName} {product.Price}");
+
+
+            Product product2 = new Product();
+
+            product2.Name = "Qelem";
+            product2.BrandName = "Oyal";
+            product2.Price = 2;
+
+            Console.WriteLine($"{product2.Name} {product2.BrandName} {product2.Price}");
+
+
+            Product product3 = new Product();
+
+            product3.Name = "Ayaqqabi";
+            product3.BrandName = "Zara";
+            product3.Price = 200;
+
+            Console.WriteLine($"{product3.Name} {product3.BrandName} {product3.Price}");
+        }
+
+    }
+}
